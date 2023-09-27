884213510b0522e07c75820a83bde08fb50a87a3
Giovani88 <l20120091@morelia.tecnm.mx>
2023-09-27 02:06:26 -0600

    Add ejercicio 2

--- estructuras de control/estructuras_control.lisp
+++ estructuras de control/estructuras_control.lisp
@@ -13,3 +13,38 @@
     )
     
 )
+
+(defun prestamo()
+    (princ "Escriba su nombre: ")
+    (setq nombre (read))
+    (princ "Cantidad solicitada: ")
+    (setq prestamo (read))
+    (princ "Salario anual: ")
+    (setq salario (read))
+    (princ "Valor de otras propiedades: ")
+    (setq propiedades (read))
+    (princ "Historia crediticia b (buena) o m (mala): ")
+    (setq historia (read))
+    (case historia
+        (m (format t "Lo sentimos ~a, no es posible otorgar el prestamo" nombre))
+        (b 
+            ;calcular punto por salario
+            (cond
+                ((and (>= salario (* 0.10 prestamo)) (< salario (* 0.25 prestamo))) (setq puntos 1))
+                ((and (>= salario (* 0.25 prestamo)) (< salario (* 0.50 prestamo))) (setq puntos 3))
+                (t (setq puntos 5))                
+            )
+            ;calcular puntos por propiedades
+            (cond 
+                ((and (>= propiedades prestamo) (< propiedades (* prestamo 2))) (setq puntos (+ puntos 3)))
+                ((>= propiedades (* prestamo 2))  (setq puntos (+ puntos 5)))
+            )
+            (if (> puntos 6) 
+                (format t "Felicidades ~a, prestamo valido" nombre) 
+                (format t "Lo sentimos ~a, no es posible otorgar el prestamo" nombre)
+            )
+        )
+        (otherwise (format t "~a No es una opcion para historia creditiica" historia))
+    )
+    
+)
